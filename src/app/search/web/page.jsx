6a9ac5eb91ba57015718a4c988e845ef763a4bc5
Tushar Koshti <tushar@zettabolt.com>
2023-12-28 16:28:13 +0530

    fetch data from google search api and showed the titles

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,10 @@
-function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items;
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
 
 export default WebSearchPage;
