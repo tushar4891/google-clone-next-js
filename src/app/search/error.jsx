ec897557fe986d4ba9d5953340d86a8d50d4929e
Tushar Koshti <tushar@zettabolt.com>
2023-12-28 17:17:18 +0530

    Handle possible errors

---
+++ src/app/search/error.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+
+function Error({ error, reset }) {
+  useEffect(() => {
+    console.log("Error : ", error);
+  }, [error]);
+  return (
+    <div className="flex flex-col justify-center items-center pt-10">
+      <h1 className="test-3xl mb-4"> Something went wrong</h1>
+      <button className="text-blue-500 " onClick={() => reset()}>
+        Try Again
+      </button>
+    </div>
+  );
+}
+
+export default Error;
