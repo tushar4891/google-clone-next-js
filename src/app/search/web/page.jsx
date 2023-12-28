64b1820a0c480371c5c3527225161b16200ee91b
Tushar Koshti <tushar@zettabolt.com>
2023-12-28 19:16:33 +0530

    Create search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
-
+import WebSearchResults from "@/Component/WebSearchResults";
 async function WebSearchPage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
@@ -21,7 +22,7 @@ async function WebSearchPage({ searchParams }) {
       </div>
     );
   }
-  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
+  return <>{results && <WebSearchResults results={data} />}</>;
 }
 
 export default WebSearchPage;
